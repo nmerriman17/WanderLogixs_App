a03ddd8cd3c20f3f2ab72ead0e533b02f697903f
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-12 22:08:28 -0600

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { searchDatabase } = require('./src/config/db.js');
 require('dotenv').config();
