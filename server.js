5e8aa1a0723de18f14844857fa87c63c778b10de
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-13 20:07:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ if (!process.env.DATABASE_URL) {
 // Database connection setup
 const poolConfig = {
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: process.env.NODE_ENV === 'production' 
+        ? { rejectUnauthorized: false } 
+        : null
 };
 
 const pool = new Pool(poolConfig);
