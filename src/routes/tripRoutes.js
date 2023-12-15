6aa3384c052d9a54adb049f29027358fbd2984e6
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-15 00:38:31 -0600

    Update tripRoutes.js

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const tripController = require('../controllers/tripController'); 
-const { authenticateToken } = require('../middleware/auth'); 
+const { authenticateToken } = require('../../middleware/auth');
 const multer = require('multer');
 const upload = multer(); 
 
