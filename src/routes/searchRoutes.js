f72257650c451f159098dd30c22a59ae8996c6a5
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-15 10:02:00 -0600

    Update searchRoutes.js

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
-const { authenticateToken } = require('../middleware/auth'); // Adjust path as needed
+const { authenticateToken } = require('../../middleware/auth');
 
 router.get('/search', authenticateToken, handleSearch);
 
